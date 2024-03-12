6420681c057f91715e7491529151ad9d345b88ed
Andrea Favia <andrea.faviait@gmail.com>
2024-03-12 10:37:27 +0100

    Apply isort + remove unused var

--- src/main.py
+++ src/main.py
@@ -1,33 +1,23 @@
-import typer
-from ocr_document.ocr_document import process_single_image, process_folder_images, to_html, to_json
-import os
+from ocr_document.ocr_document import process_folder_images
+from ocr_document.ocr_document import process_single_image
+from ocr_document.ocr_document import to_html
+from ocr_document.ocr_document import to_json
 from typing import Optional
+
+import os
+import typer
 
 app = typer.Typer()
 
 @app.command()
-def greet(name: str = "World", enthusiastic: bool = False):
-    """
-    Greet the user.
-    """
-    greeting = "Hello" if not enthusiastic else "Helloooooo"
-    message = f"{greeting}, {name}!"
-
-    typer.echo(message)
-
-@app.command()
 def id2json(*, file_path: Optional[str] = None ,output_path: Optional[str] = None, path_folder: Optional[str] = None, save_html: bool = False):
     """
-    Convert an image to json or html
-
+    Convert an image to json or html using OCR + Mistral LLM for RAG Extraction
     """
-    #check either file_path or path_folder are not none
     if not file_path and not path_folder:
         raise typer.BadParameter("Please provide either file_path or path_folder")
-    #check if file_path, output_path point to exististing files if they are not None
     if file_path and not os.path.exists(file_path):
         raise typer.BadParameter(f"File {file_path} does not exist")
-    #check if path_folder is a directory
     if path_folder and not os.path.isdir(path_folder):
         raise typer.BadParameter(f"{path_folder} is not a directory")
     
