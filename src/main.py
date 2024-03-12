e8c65874504c5afbf2f0767c34cbbbf4e61c5e7e
Andrea Favia <andrea.faviait@gmail.com>
2024-03-12 18:07:51 +0100

    Add merge_txt_files  command

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from ocr_document.ocr_document import process_folder_images
 from ocr_document.ocr_document import process_single_image
 from ocr_document.ocr_document import to_html
 from ocr_document.ocr_document import to_json
+from merge_txt_files.merge_txt_files import merge_txt_files
 from typing import Optional
 
 import os
@@ -44,14 +45,21 @@ def id2json(
             results,
             out_file,
         )
-    
+
     typer.echo(f"Output file saved at {out_file}")
 
 
 @app.command()
-def merge_txts(*, folder_path: str):
-    pass
+def merge_txt(*, folder_path: str, output_path: Optional[str] = "output_merged.txt"):
+    """ Merge .txt files by appending one row after the other"""
+    if not folder_path or not os.path.isdir(folder_path):
+        raise typer.BadParameter("Please provide a valid path folder")
+    if not isinstance(output_path, str) or not output_path.endswith(".txt"):
+        raise typer.BadParameter("Output Path must be a string with .txt as format")
 
+    merged_document: str = merge_txt_files(path=folder_path, output_path=output_path)
+
+    typer.echo(f"File saved at {merged_document}")
 
 
 if __name__ == "__main__":
